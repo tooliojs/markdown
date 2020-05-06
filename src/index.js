b13a42aa6cc9410f90b05abe7eb0a00c02e08a81
arakilian0 <arakilian0@gmail.com>
2020-05-06 12:16:17 -0700

    UPDATE initial building blocks of markdown package

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
-module.exports = function() {
-    console.log('running markdown')
+const marked = require('marked')
+const _sanitize = function(input, options=null) { return input }
+let sanitize = require('sanitize-html')
+
+try {
+    const pkg = require(require('path').resolve('package.json'))
+    if(pkg.toolio && pkg.toolio.markdown && pkg.toolio.markdown.sanitize === false) sanitize = _sanitize
+}
+catch(err) { return new Error(err) }
+
+module.exports = function(input, options, __sanitize=true) {
+    if(__sanitize === false) sanitize = _sanitize
+    if(!input) return undefined
+    if(!options) return sanitize(marked(input))
+    else {
+        if(!options.marked || !options.sanitizeHtml) return sanitize(marked(input))
+        else {
+            if(options.marked && options.sanitizeHtml)        { marked.setOptions(options.marked); return sanitize(marked(input), options.sanitizeHtml) }
+            else if(options.marked && !options.sanitizeHtml)  { marked.setOptions(options.marked); return sanitize(marked(input)) }
+            else if(!options.marked && options.sanitizeHtml)  { return sanitize(marked(input), options.sanitizeHtml) }
+        }
+    }
 }
--- test/index.js
+++ test/index.js
@@ -1,3 +1,15 @@
-const markdown = require('../src')
+const markdown = require('../src')(`
+#### Testing Bruh
+I kinda fell in love with Markdown.
 
-console.log(markdown())
+[let's link](github.com/arakilian0)
+`, {
+    marked: {
+        test: true
+    },
+    sanitizeHtml: {
+        test: true
+    }
+}, true)
+
+console.log(markdown)
