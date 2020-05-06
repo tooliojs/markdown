8815fd275da78714f3c130ec61a8df611eaca858
arakilian0 <arakilian0@gmail.com>
2020-05-06 13:30:53 -0700

    REMOVED package.json option setter

--- src/index.js
+++ src/index.js
@@ -2,12 +2,6 @@ const marked = require('marked')
 const _sanitize = function(input, options=null) { return input }
 let sanitize = require('sanitize-html')
 
-try {
-    const pkg = require(require('path').resolve('package.json'))
-    if(pkg.toolio && pkg.toolio.markdown && pkg.toolio.markdown.sanitize === false) sanitize = _sanitize
-}
-catch(err) { return new Error(err) }
-
 module.exports = function(input, options, __sanitize=true) {
     if(__sanitize === false) sanitize = _sanitize
     if(!input) return undefined
